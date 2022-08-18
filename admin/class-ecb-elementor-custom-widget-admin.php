f76b149f8e3578c3f8a0c86a92514146da13ebed
Miguel Joshua Dayao <88497417+migueldayaovont@users.noreply.github.com>
2022-08-18 16:43:45 +0800

    ACF Field to Code

--- admin/class-ecb-elementor-custom-widget-admin.php
+++ admin/class-ecb-elementor-custom-widget-admin.php
@@ -83,7 +83,6 @@ class Ecb_Elementor_Custom_Widget_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
 		/**
 		 * This function is provided for demonstration purposes only.
 		 *
@@ -97,6 +96,130 @@ class Ecb_Elementor_Custom_Widget_Admin {
 		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ecb-elementor-custom-widget-admin.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+	/**
+	 * Register ACF Code
+	 *
+	 * Register new ACF Code.
+	 *
+	 * @since 1.2.0
+	 * @access public
+	 *
+	 */
+	public function register_acf_fields() {
+		
+		if( function_exists('acf_add_local_field_group') ):
+			acf_add_local_field_group(array(
+				'key' => 'group_62fcfb4d649ec',
+				'title' => 'Pages',
+				'fields' => array(
+					array(
+						'key' => 'field_62fcfb5296c40',
+						'label' => 'Ecb Text',
+						'name' => 'ecb_text',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+					),
+					array(
+						'key' => 'field_62fdb3cb55afd',
+						'label' => 'Ecb Repeater',
+						'name' => 'ecb_repeater',
+						'type' => 'repeater',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'collapsed' => '',
+						'min' => 0,
+						'max' => 0,
+						'layout' => 'table',
+						'button_label' => '',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_62fdb44d55aff',
+								'label' => 'ECB Image',
+								'name' => 'ecb_image',
+								'type' => 'image',
+								'instructions' => '',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'return_format' => 'url',
+								'preview_size' => 'medium',
+								'library' => 'all',
+								'min_width' => '',
+								'min_height' => '',
+								'min_size' => '',
+								'max_width' => '',
+								'max_height' => '',
+								'max_size' => '',
+								'mime_types' => '',
+							),
+							array(
+								'key' => 'field_62fdb3f855afe',
+								'label' => 'ECB Description',
+								'name' => 'ecb_description',
+								'type' => 'textarea',
+								'instructions' => '',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'maxlength' => '',
+								'rows' => '',
+								'new_lines' => '',
+							),
+						),
+					),
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'post_type',
+							'operator' => '==',
+							'value' => 'page',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => true,
+				'description' => '',
+				'show_in_rest' => 0,
+			));
+			
+			endif;		
 
 	}
 
